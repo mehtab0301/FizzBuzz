a352cc69e06812a77bf4ca9aa64405567e2901f7
mehtab0301 <rida.asif@mail.utoronto.ca>
2023-09-11 16:24:35 -0400

    hi

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -7,11 +7,15 @@ class FizzBuzz {
 
         for (int i = 1; i < 100; i++) {
 
-            // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
-            boolean divisibleBy5 = i % 5 == 0;
+            doFizz(i);
+        }
+      
+        int j = 1;
 
-            // Print our appropriate result.
+        while (j < 100){
+            boolean divisibleBy3 = j % 3 == 0;
+            boolean divisibleBy5 = j % 5 == 0;
+
             if (divisibleBy3 && divisibleBy5) {
 
                 System.out.println("Fizz Buzz");
@@ -26,9 +30,38 @@ class FizzBuzz {
 
             } else {
 
-                System.out.println(i);
+                System.out.println(j);
 
             }
+
+            j++;
+
+
+        }
+    }
+
+    private static void doFizz(int i) {
+        // Find out which numbers divide i.
+        boolean divisibleBy3 = i % 3 == 0;
+        boolean divisibleBy5 = i % 5 == 0;
+
+        // Print our appropriate result.
+        if (divisibleBy3 && divisibleBy5) {
+
+            System.out.println("Fizz Buzz");
+
+        } else if (divisibleBy3) {
+
+            System.out.println("Fizz");
+
+        } else if (divisibleBy5) {
+
+            System.out.println("Buzz");
+
+        } else {
+
+            System.out.println(i);
+
         }
     }
 }
